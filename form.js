ac20a62762a552b03da047ffc866b892efee2f2e
tonyroumi <122657864+anthony-roumi@users.noreply.github.com>
2023-11-28 19:52:19 -0800

    darkmodeIcons

--- form.js
+++ form.js
@@ -1,18 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.getElementById('checkbox');
   const htmlTag = document.querySelector('html');
+  const mailImg = document.querySelector('nav img');
+  const linkedImg = document.querySelector('nav li:last-child img');
 
   let savedTheme = localStorage.getItem('theme');
   if(savedTheme != null) {
     htmlTag.setAttribute('data-theme', savedTheme);
-
+  }
+  if(savedTheme === 'dark') {
+    mailImg.setAttribute('src', '/images/contacticons/mailwhite.png');
+    linkedImg.setAttribute('src', '/images/contacticons/linkedinwhite.png');
+  } else {
+    mailImg.setAttribute('src', '/images/contacticons/mailicon.png');  
+    linkedImg.setAttribute('src', '/images/contacticons/linkedinicon.png');
   }
   checkbox.checked = savedTheme === 'dark';
   checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
       htmlTag.setAttribute('data-theme', 'dark');
+      mailImg.setAttribute('src', '/images/contacticons/mailwhite.png');
+      linkedImg.setAttribute('src', '/images/contacticons/linkedinwhite.png');
     } else {
-      htmlTag.setAttribute('data-theme', 'light')
+      htmlTag.setAttribute('data-theme', 'light');
+      mailImg.setAttribute('src', '/images/contacticons/mailicon.png'); 
+      linkedImg.setAttribute('src', '/images/contacticons/linkedinicon.png');
     }
     localStorage.setItem('theme', htmlTag.getAttribute('data-theme'));
   });
